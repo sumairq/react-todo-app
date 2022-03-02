005d33f3534e7fb373c040ff3131c6be00109595
Sumair <sumairqaisar992@gmail.com>
2022-03-03 00:06:05 +0500

    Application with functional components and hooks

--- src/classBased/components/TodoItem.js
+++ src/classBased/components/TodoItem.js
@@ -2,7 +2,27 @@ import React from "react";
 import styles from "./TodoItem.module.css"
 
 class TodoItem extends React.Component {
+
+    state = {
+        editing: false,
+      }
+
+    handleEditing = () => {
+       this.setState({
+           editing: true,
+       })
+      }
     
+      handleUpdatedDone = event => {
+        if (event.key === "Enter") {
+            this.setState({ editing: false })
+          }
+      }
+
+      componentWillUnmount() {
+        console.log("Cleaning up...")
+      }
+
     render(){
         const completedStyle = {
             fontStyle: "italic",
@@ -11,9 +31,17 @@ class TodoItem extends React.Component {
             textDecoration: "line-through",
           }
           const { completed, id, title } = this.props.todo
-
+          let viewMode = {}
+          let editMode = {}
+          
+          if (this.state.editing) {
+            viewMode.display = "none"
+          } else {
+            editMode.display = "none"
+          }
         return (
             <li className={styles.item}>
+                <div onDoubleClick={this.handleEditing} style={viewMode}>
               <input
                 type="checkbox"
                 className={styles.checkbox}
@@ -26,7 +54,10 @@ class TodoItem extends React.Component {
               <span style={completed ? completedStyle : null}>
               {title}
               </span>
-
+              </div>
+              <input type="text" style={editMode} className={styles.textInput} value={title} onChange={e => {
+    this.props.setUpdate(e.target.value, id)
+  }} onKeyDown={this.handleUpdatedDone} />
             </li>
           )
 
